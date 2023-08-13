ef4efad2f25ce2c298812fe4cf263b73476a00be
Zhora-madcurve <h.asauliak@madcurve.com>
2023-08-13 06:29:25 +0300

    feat(PF-4): swagger

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,8 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 
 
 export class CreatePostDto {
+    @ApiProperty({ example: 'How to learn js?', description: 'Post name' })
     readonly title: string;
+    @ApiProperty({ example: 'Tell me what ways you found the strength to learn js?', description: 'Text' })
     readonly content: string;
+    @ApiProperty({ example: 1, description: 'user id' })
     readonly userId: number;
     
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/c
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostService } from './posts.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Posts')
 @Controller('posts')
 export class PostController {
 
